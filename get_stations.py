7bfe99c2fe30e41363607b19284c676da7c5f8ec
mcallandriello <meghan.d.callandriello@vanderbilt.edu>
2021-12-05 12:30:49 -0600

    Limit number of unique stations to 10 in the Nashville area

--- get_stations.py
+++ get_stations.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 
 def point_in_tn(lat: float, lon: float) -> bool:
-    LEFT_LONG_TN = -90.5
-    RIGHT_LONG_TN = -81.5
-    TOP_LAT_TN = 36.7
-    BOT_LAT_TN = 34.9
+    LEFT_LONG_TN = -88.00
+    RIGHT_LONG_TN = -85.00
+    TOP_LAT_TN = 36.49
+    BOT_LAT_TN = 35.55
     if (BOT_LAT_TN < lat < TOP_LAT_TN) and (LEFT_LONG_TN < lon < RIGHT_LONG_TN):
         return True
     else:
@@ -15,10 +15,10 @@ def point_in_tn(lat: float, lon: float) -> bool:
 df = pd.read_csv("stations_stable.csv")
 df = df[df["country_code"] == "US"]
 df = df[df["frequency"] == "current only"]
-LEFT_LONG_TN = -90.5
-RIGHT_LONG_TN = -81.5
-TOP_LAT_TN = 36.7
-BOT_LAT_TN = 34.9
+LEFT_LONG_TN = -88.00
+RIGHT_LONG_TN = -85.00
+TOP_LAT_TN = 36.49
+BOT_LAT_TN = 35.55
 df = df[(BOT_LAT_TN < df["lat"]) & (df["lat"] < TOP_LAT_TN) &
         (LEFT_LONG_TN < df["lon"]) & (df["lon"] < RIGHT_LONG_TN)]
 print(f"unique stations in TN = {len(df['station_id'].unique())}")
