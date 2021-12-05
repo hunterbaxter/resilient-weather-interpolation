490c3cfab915422eb978383fbf25fc14faeae42b
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2021-12-05 14:19:50 -0600

    feat: stations used, dockerized kafka playbook, dockerfile for treiver

--- get_stations.py
+++ get_stations.py
@@ -1,24 +1,16 @@
 import pandas as pd
 
-
-def point_in_tn(lat: float, lon: float) -> bool:
-    LEFT_LONG_TN = -88.00
-    RIGHT_LONG_TN = -85.00
-    TOP_LAT_TN = 36.49
-    BOT_LAT_TN = 35.55
-    if (BOT_LAT_TN < lat < TOP_LAT_TN) and (LEFT_LONG_TN < lon < RIGHT_LONG_TN):
-        return True
-    else:
-        return False
-
-
+# NOTE: File downloaded from weatherbit. Would be nice to have it automated.
 df = pd.read_csv("stations_stable.csv")
 df = df[df["country_code"] == "US"]
 df = df[df["frequency"] == "current only"]
-LEFT_LONG_TN = -88.00
-RIGHT_LONG_TN = -85.00
-TOP_LAT_TN = 36.49
-BOT_LAT_TN = 35.55
-df = df[(BOT_LAT_TN < df["lat"]) & (df["lat"] < TOP_LAT_TN) &
-        (LEFT_LONG_TN < df["lon"]) & (df["lon"] < RIGHT_LONG_TN)]
-print(f"unique stations in TN = {len(df['station_id'].unique())}")
+LEFT_LONG = -88.00
+RIGHT_LONG = -85.00
+TOP_LAT = 36.49
+BOT_LAT = 35.55
+df = df[(BOT_LAT < df["lat"]) & (df["lat"] < TOP_LAT) &
+        (LEFT_LONG < df["lon"]) & (df["lon"] < RIGHT_LONG)]
+print(f"unique stations in region = {len(df['station_id'].unique())}")
+df[["station_id", "lat", "lon"]].to_csv("stations_production.csv", index=False)
+df[["station_id", "lat", "lon"]] \
+    .head(2).to_csv("stations_test.csv", index=False)
