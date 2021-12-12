73c36d3a3ded4b2c3db0ce8eeb42463b05434f4f
alexpena9291 <alexpena9291@gmail.com>
2021-12-11 21:15:08 -0600

    feat(api): basic scaffolding for pulling from kafka

--- api/kafka_tools.py
+++ api/kafka_tools.py
@@ -1,4 +1,4 @@
-# from kafka import KafkaConsumer
+from kafka import KafkaConsumer
 from typing import List
 from pandas import Timestamp
 import numpy as np
@@ -9,56 +9,233 @@ from scipy.interpolate import griddata
 
 dummy_data = {
     'center1': [
-        {'measurement': 'weather',
-         'tags': {'lat': 36.15, 'lon': -86.79},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 36.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.15, 'lon': -86.79},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 37.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.13, 'lon': -86.79},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 38.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}}
+        {
+            "rh": 68,
+            "pod": "n",
+            "lon": -86.79,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.15,
+            "weather": {
+                "icon": "c04n",
+                "code": 804,
+                "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 69,
+            "pod": "n",
+            "lon": -86.79,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.15,
+            "weather": {
+                "icon": "c04n",
+                "code": 804,
+                "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },                {
+            "rh": 70,
+            "pod": "n",
+            "lon": -86.79,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.15,
+            "weather": {
+                "icon": "c04n",
+                "code": 804,
+                "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
     ],
     'center2': [
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.77},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 45.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.77},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 44.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.77},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 43.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.77},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 42.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}}
+        {
+            "rh": 58,
+            "pod": "n",
+            "lon": -86.77,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 59,
+            "pod": "n",
+            "lon": -86.77,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 60,
+            "pod": "n",
+            "lon": -86.77,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 61,
+            "pod": "n",
+            "lon": -86.77,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+
     ],
     'center3': [
-        {'measurement': 'weather',
-         'tags': {'lat': 36.13, 'lon': -86.78},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 1.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.13, 'lon': -86.78},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 2.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
+
+        {
+            "rh": 48,
+            "pod": "n",
+            "lon": -86.78,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.13,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 49,
+            "pod": "n",
+            "lon": -86.78,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.13,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
     ],
     'center4': [
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.8},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 1.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
-        {'measurement': 'weather',
-         'tags': {'lat': 36.14, 'lon': -86.8},
-         'time': Timestamp('2021-11-28 03:30:00+0000', tz='UTC'),
-         'fields': {'wind_cdir': 'SSW', 'rh': 2.0, 'pod': 'n', 'pres': 994.3, 'timezone': 'America/Chicago', 'country_code': 'US', 'clouds': 75.0, 'vis': 5.0, 'wind_spd': 2.06, 'wind_cdir_full': 'south-southwest', 'app_temp': 8.0, 'state_code': 'TN', 'h_angle': -90.0, 'dewpt': -6.3, 'weather_icon': 'c01n', 'weather_code': 800, 'weather_description': 'Clear sky', 'uv': 0.0, 'aqi': 23.0, 'station': 'KBNA', 'wind_dir': 200.0, 'elev_angle': -53.32, 'precip': 0.0, 'ghi': 0.0, 'dni': 0.0, 'dhi': 0.0, 'solar_rad': 0.0, 'city_name': 'Berry Hill', 'sunrise': '12:36', 'sunset': '22:33', 'temp': 8.0, 'slp': 1016.3}},
+        {
+            "rh": 55,
+            "pod": "n",
+            "lon": -86.8,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
+        {
+            "rh": 56,
+            "pod": "n",
+            "lon": -86.8,
+            "pres": 994.3,
+            "timezone": "America/Chicago",
+            "ob_time": "2021-12-12 00:50",
+            "aqi": 18,
+            "lat": 36.14,
+            "weather": {
+                        "icon": "c04n",
+                        "code": 804,
+                        "description": "Overcast clouds"
+            },
+            "datetime": "2021-12-12:01",
+            "temp": 5.9,
+            "station": "0505W",
+            "elev_angle": -28.46,
+            "app_temp": 5.9
+        },
     ]
 }
 
@@ -70,17 +247,24 @@ class KafkaWeather():
         self.number = 0
 
         # Setting up dictionary to hold most recent weather info
-        self.built_station_dict_from_list(station_list)
+        self.build_station_dict_from_list(station_list)
 
         scheduler = BackgroundScheduler()
         scheduler.add_job(func=self.update_map, trigger="interval", seconds=10)
         scheduler.start()
         self.update_map()
 
-    def built_station_dict_from_list(self, station_list):
+    def build_station_dict_from_list(self, station_list):
         for station in station_list:
             self.station_dict[station] = {
                 'latest_data': None, 'topic': dummy_data[station]}
+
+    def build_station_dict_from_kafka(self, station_list, ip):
+        for station in station_list:
+            listener = KafkaConsumer(bootstrap_servers=ip)
+            listener.subscribe(topics=[str(station)])
+            self.station_dict[station] = {
+                'latest_data': None, 'topic': listener}
 
     def get_formatted_data_stream(self):
         data_dictionary = {"type": "FeatureCollection", "features": []}
@@ -105,14 +289,21 @@ class KafkaWeather():
     def updated_value(self, station_properties):
         return self.get_latest_value_from_list(station_properties['topic'])
 
+    def get_latest_value_from_kafka(self, kafka_consumer):
+        try:
+            val = next(kafka_consumer)
+            return val
+        except:
+            return None
+
     def get_latest_value_from_list(self, lst: List):
         return (lst.pop(0) if len(lst) > 0 else None)
 
     def simplify_data(self, data, field='rh'):
         return {
-            'lat': data['tags']['lat'],
-            'lon': data['tags']['lon'],
-            'value': data['fields'][field]
+            'lat': data['lat'],
+            'lon': data['lon'],
+            'value': data[field]
         }
 
     def format_data(self, data):
