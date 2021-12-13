1baf504feceb1796f3fe3fbb2c577e4d80c45942
jdnhix <jdhix98890@yahoo.com>
2021-12-13 02:27:05 -0600

    remove webpack implementation, add dockerfile

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,23 +1,3 @@
-// Copyright (c) 2021 Uber Technologies, Inc.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
@@ -25,11 +5,12 @@ import styled from 'styled-components';
 import KeplerGl from 'kepler.gl';
 import {addDataToMap, wrapTo} from 'kepler.gl/actions';
 import {processGeojson} from 'kepler.gl/processors';
+import config from './config';
+import axios from 'axios'
 
-const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+const MAPBOX_TOKEN = "pk.eyJ1IjoiamRuaGl4IiwiYSI6ImNrd2s0NzF2ejFvb20yeW80NTlvenJ4OXMifQ.FxlxjZc-nN21l0s7N6EUwA"
 
-
-const sampledata = {
+var geojson = {
   "type":"FeatureCollection",
   "features":[
      {
@@ -154,96 +135,6 @@ const sampledata = {
   ]
 }
 
-const sampleconfig = {
-  visState: {
-    layers: [
-      {
-        id: "321",
-        type: "geojson",
-        config: {
-          dataId: 'weather_data',
-          label: "Weather",
-          columns: {
-            geojson: "_geojson"
-          },
-          isVisible: true,
-          visConfig: {
-            opacity: 0.55,
-            thickness: 2,
-            colorRange: {
-              name: "Test Range",
-              type: "sequential",
-              category: "Uber",
-              colors: [
-                "#DBE9F5",
-                "#AFC6D9",
-                "#83A3BE",
-                "#5880A2",
-                "#2C5D87",
-                "#003A6B"
-              ],
-              reversed: false
-            },
-            radius: 10,
-            stroked: false,
-            filled: true,
-            tempRange: [
-              0, 10
-            ]
-          },
-          visualChannels: {
-            colorField: {
-              name: "temp",
-              type: "int"
-            },
-            colorScale: "quantile",
-            // tempField: {
-            //   name: "temp",
-            //   type: "integer"
-            // },
-            // tempScale: "linear"
-          }
-        }
-      }
-    ]
-  },
-  mapState: {
-    latitude: 36.1627,
-    longitude: -86.78,
-    zoom: 7,
-  },
-  mapStyle: {
-    styleType: 'dark',
-    // topLayerGroups: {},
-    visibleLayerGroups: {
-      label: true,
-      road: true,
-      border: false,
-      // building: true,
-      water: true,
-      land: true
-    }
-  }
-}
-
-const sample = {
-    datasets: {
-      info: {
-        label: 'input info',
-        id: 'weather_data'
-      },
-      data: processGeojson(sampledata)
-    },
-    options: {
-      centerMap: false,
-      readOnly: false
-    },
-    config: sampleconfig
-}
-
-
-
-
 const StyledWrapper = styled.div`
   position: absolute;
   width: 100vw;
@@ -251,11 +142,34 @@ const StyledWrapper = styled.div`
 `;
 
 const wrapToMap = wrapTo('map1')
-const App = ({dispatch}) => {
+const apiURL = "http://3.144.203.184:8080/kepler/data"
+
+function App({dispatch}) {
 
   useEffect(()=> {
-    dispatch(wrapToMap(addDataToMap(sample)))
-  }, [sampledata])
+    const interval = setInterval(() => {
+      axios.get(apiURL).then(res => {
+        console.log(res.data)
+
+        var data = {
+          datasets: {
+            info: {
+              label: 'input info',
+              id: 'weather_data'
+            },
+            data: processGeojson(res.data)
+          },
+          options: {
+            centerMap: false,
+            readOnly: false
+          },
+          config: config
+        }
+
+        dispatch(wrapToMap(addDataToMap(data)))
+      })
+    }, 10000)
+  }, [])
 
   return(
     <StyledWrapper>
@@ -266,7 +180,6 @@ const App = ({dispatch}) => {
       </AutoSizer>
     </StyledWrapper>
   );
-
 }
 
 const mapStateToProps = state => state;
